ae06c6e051d61e4567c7f66a209cc0f7fe125439
kaixzhou <quic_kaixzhou@quicinc.com>
2024-11-01 13:43:23 +0530

    qcx-kernel: Fix Compilation Issue.
    1. Comment qos_settings for passing the compilation.
    2. Change cmd_i2c_burst data type from uint32_t to uint8_t to match with
    sensorsdk.h in cdk_qcx.
    
    Change-Id: Icc089fc15ee42cc6dd57e0a9654c371b70573e62
    Signed-off-by: kaixzhou <quic_kaixzhou@quicinc.com>

--- drivers/cam_utils/cam_compat.c
+++ drivers/cam_utils/cam_compat.c
@@ -213,20 +213,6 @@ void cam_free_clear(const void * ptr)
 }
 #endif
 
-int cam_update_camnoc_qos_settings(uint32_t use_case_id,
-	uint32_t qos_cnt, struct qcom_scm_camera_qos *scm_buf)
-{
-	int rc = 0;
-
-	rc = qcom_scm_camera_update_camnoc_qos(use_case_id, qos_cnt, scm_buf);
-	if (rc) {
-		CAM_ERR(CAM_ISP, "scm call to update QoS failed: %d", rc);
-		rc = -EINVAL;
-	}
-
-	return rc;
-}
-
 /* Callback to compare device from match list before adding as component */
 static inline int camera_component_compare_dev(struct device *dev, void *data)
 {
